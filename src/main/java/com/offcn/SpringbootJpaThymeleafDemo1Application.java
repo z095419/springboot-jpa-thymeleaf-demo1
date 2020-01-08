c2c1784ee05938aba19e50cb21ac144d961c9e83
zou <807699398@qq.com>
2020-01-08 19:53:06 +0800

    新增注释

--- src/main/java/com/offcn/SpringbootJpaThymeleafDemo1Application.java
+++ src/main/java/com/offcn/SpringbootJpaThymeleafDemo1Application.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootJpaThymeleafDemo1Application {
-
+    /**
+     * 新增注释***************
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(SpringbootJpaThymeleafDemo1Application.class, args);
     }
