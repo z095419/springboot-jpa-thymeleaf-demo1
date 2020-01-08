f7a2c25cdd8a1dd77a7f005290f0d8ac9accb4f9
z095419 <48671989+z095419@users.noreply.github.com>
2020-01-08 19:57:02 +0800

    Update SpringbootJpaThymeleafDemo1Application.java

--- src/main/java/com/offcn/SpringbootJpaThymeleafDemo1Application.java
+++ src/main/java/com/offcn/SpringbootJpaThymeleafDemo1Application.java
@@ -3,6 +3,7 @@ package com.offcn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//**************************************************
 @SpringBootApplication
 public class SpringbootJpaThymeleafDemo1Application {
     /**
